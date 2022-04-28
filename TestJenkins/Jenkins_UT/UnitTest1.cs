48afd27d83bdc931c59ccc3f08a358bc8f5b6048
Gaetan <gaetan020998@gmail.com>
2022-04-28 20:52:33 +0200

    Création projet

---
+++ TestJenkins/Jenkins_UT/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jenkins_UT
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
